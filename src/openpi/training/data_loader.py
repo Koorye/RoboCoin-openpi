6ee94467ea6cdc4300b0e319cae3d3492d025dc6
Koorye <a1311586225@gmail.com>
2025-09-15 11:32:57 +0800

    update

--- src/openpi/training/data_loader.py
+++ src/openpi/training/data_loader.py
@@ -146,7 +146,7 @@ def create_torch_dataset(
         },
         video_backend='pyav',
     )
-    dataset = LeRobotDatasetWithAnnotations(dataset)
+    dataset = LeRobotDatasetWithAnnotations(dataset, use_annotation=use_annotation)
 
     if data_config.prompt_from_task:
         dataset = TransformedDataset(dataset, [_transforms.PromptFromLeRobotTask(dataset_meta.tasks)])
